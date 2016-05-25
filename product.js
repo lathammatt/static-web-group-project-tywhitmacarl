1931738a1b383620e96141a66c08ca96f30105fb
Matt Latham <latham75@gmail.com>
2016-05-25 09:47:19 -0500

    corrected syntax to js

--- product.js
+++ product.js
@@ -1,52 +1,52 @@
 var products = [
 	{
-		name: giraffe;
-		description: perfect for safaris;
-		price: $500;
-		size: large;
-		URL: <img src="img/giraffe.jpg"
+		name: "giraffe";
+		description: "perfect for safaris";
+		price: "$500";
+		size: "large";
+		URL: "<img src="img/giraffe.jpg">"
 	},	{
-		name: basketball;
-		description: for the sports fanatic;
-		price: $600;
-		size: small;
-		URL: <img src="img/basketball.jpg"
+		name: "basketball";
+		description: "for the sports fanatic";
+		price: "$600";
+		size: "small";
+		URL: "<img src="img/basketball.jpg">"
 	},	{
-		name: batman;
-		description: for chasing catwoman;
-		price: $300;
-		size: medium;
-		URL: <img src="img/batman.jpg"
+		name: "batman";
+		description: "for chasing catwoman";
+		price: "$300";
+		size: "medium";
+		URL: "<img src="img/batman.jpg">"
 	},	{
-		name: christmas;
-		description: get into the howl-iday spirit;
-		price: $400;
-		size: large;
-		URL: <img src="img/christmas.jpg"
+		name: "christmas";
+		description: "get into the howl-iday spirit";
+		price: "$400";
+		size: "large";
+		URL: "<img src="img/christmas.jpg">"
 	},	{
-		name: clouds;
-		description: for heavenly comfort;
-		price: $250;
-		size: small;
-		URL: <img src="img/clouds.jpg"
+		name: "clouds";
+		description: "for heavenly comfort";
+		price: "$250";
+		size: "small";
+		URL: "<img src="img/clouds.jpg">"
 	},	{
-		name: dots;
-		description: for dalmation wannabes;
-		price: $150;
-		size: small;
-		URL: <img src="img/dots.jpg"
+		name: "dots";
+		description: "for dalmation wannabes";
+		price: "$150";
+		size: "small";
+		URL: "<img src="img/dots.jpg">"
 	},	{
-		name: duck;
-		description: when you wish you had a different pet;
-		price: $300;
-		size: small;
-		URL: <img src="img/duck.jpg"
+		name: "duck";
+		description: "when you wish you had a different pet";
+		price: "$300";
+		size: "small";
+		URL: "<img src="img/duck.jpg">"
 	},	{
-		name: stripes;
-		description: stylish and debonair;
-		price: $400;
-		size: medium;
-		URL: <img src="img/stripes.jpg"
+		name: "stripes";
+		description: "stylish and debonair";
+		price: "$400";
+		size: "medium";
+		URL: "<img src="img/stripes.jpg">"
 	},
 ]
 
