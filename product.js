aa6585f7891e8c235ac9db47533e946f3f853beb
Matt Latham <latham75@gmail.com>
2016-05-25 11:33:53 -0500

    reworked

--- product.js
+++ product.js
@@ -1,52 +1,64 @@
 var products = [
 	{
-		name: "giraffe";
-		description: "perfect for safaris";
-		price: "$500";
-		size: "large";
-		URL: "<img src="img/giraffe.jpg">"
+		name: "Giraffe",
+		description: "perfect for safaris",
+		price: "$500",
+		size: "large",
+		URL: '<img src="img/giraffe.jpg">'
 	},	{
-		name: "basketball";
-		description: "for the sports fanatic";
-		price: "$600";
-		size: "small";
-		URL: "<img src="img/basketball.jpg">"
+		name: "Basketball",
+		description: "for the sports fanatic",
+		price: "$600",
+		size: "small",
+		URL: '<img src="img/basketball.jpg">'
 	},	{
-		name: "batman";
-		description: "for chasing catwoman";
-		price: "$300";
-		size: "medium";
-		URL: "<img src="img/batman.jpg">"
+		name: "Batman",
+		description: "for chasing catwoman",
+		price: "$300",
+		size: "medium",
+		URL: '<img src="img/batman.jpg">'
 	},	{
-		name: "christmas";
-		description: "get into the howl-iday spirit";
-		price: "$400";
-		size: "large";
-		URL: "<img src="img/christmas.jpg">"
+		name: "Christmas",
+		description: "get into the howl-iday spirit",
+		price: "$400",
+		size: "large",
+		URL: '<img src="img/christmas.jpeg">'
 	},	{
-		name: "clouds";
-		description: "for heavenly comfort";
-		price: "$250";
-		size: "small";
-		URL: "<img src="img/clouds.jpg">"
+		name: "Clouds",
+		description: "for heavenly comfort",
+		price: "$250",
+		size: "small",
+		URL: '<img src="img/clouds.jpg">'
 	},	{
-		name: "dots";
-		description: "for dalmation wannabes";
-		price: "$150";
-		size: "small";
-		URL: "<img src="img/dots.jpg">"
+		name: "Dots",
+		description: "for dalmation wannabes",
+		price: "$150",
+		size: "small",
+		URL: '<img src="img/dots.jpg">'
 	},	{
-		name: "duck";
-		description: "when you wish you had a different pet";
-		price: "$300";
-		size: "small";
-		URL: "<img src="img/duck.jpg">"
+		name: "Duck",
+		description: "when you wish you had a different pet",
+		price: "$300",
+		size: "small",
+		URL: '<img src="img/duck.jpg">'
 	},	{
-		name: "stripes";
-		description: "stylish and debonair";
-		price: "$400";
-		size: "medium";
-		URL: "<img src="img/stripes.jpg">"
+		name: "Stripes",
+		description: "stylish and debonair",
+		price: "$400",
+		size: "medium",
+		URL: '<img src="img/stripes.jpg">'
 	},
 ]
+
+var productInfo = document.getElementById("dogs")
+
+for (var i = 0; i < products.length; i++) {
+	var pName = products[i].name;
+	var pDesc = products[i].description;
+	var pPrice = products[i].price;
+	var pSize = products[i].size;
+	var pURL = products[i].URL;
+	productInfo.innerHTML += "<div class='productContainer'>" + "<div class='dogpic'>" + pURL +"</div>" + "<div class='dogname'>" + pName +"</div>" + "<div class='dogdesc'>" + pDesc +"</div>" +"<div class='dogprice'>" + pPrice +"</div>" +"<div class='dogsize'>" + pSize +"</div>" + "</div>"
+}
+
 
